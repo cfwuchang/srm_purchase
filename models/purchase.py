dffbe226aae413507f88e76b471b318c5b28d6b8
cfwuchang <67139173+cfwuchang@users.noreply.github.com>
2022-06-08 11:02:59 +0800

    Update purchase.py

--- models/purchase.py
+++ models/purchase.py
@@ -100,25 +100,25 @@ class PurchaseOrder(models.Model):
         return res
 
     
-    def action_view_picking(self):
-        '''
-        This function returns an action that display existing picking orders of given purchase order ids.
-        When only one found, show the picking immediately.
-        '''
-        action = self.env.ref('stock.action_picking_tree_all')
-        result = action.read()[0]
+#     def action_view_picking(self):
+#         '''
+#         This function returns an action that display existing picking orders of given purchase order ids.
+#         When only one found, show the picking immediately.
+#         '''
+#         action = self.env.ref('stock.action_picking_tree_all')
+#         result = action.read()[0]
 
-        # override the context to get rid of the default filtering on operation type
-        result['context'] = {}
-        pick_ids = self.mapped('picking_ids')
-        # choose the view_mode accordingly
-        if not pick_ids or len(pick_ids) > 1:
-            result['domain'] = "[('id','in',%s), ('state', '!=', 'cancel')]" % (pick_ids.ids)
-        elif len(pick_ids) == 1:
-            res = self.env.ref('stock.view_picking_form', False)
-            result['views'] = [(res and res.id or False, 'form')]
-            result['res_id'] = pick_ids.id
-        return result
+#         # override the context to get rid of the default filtering on operation type
+#         result['context'] = {}
+#         pick_ids = self.mapped('picking_ids')
+#         # choose the view_mode accordingly
+#         if not pick_ids or len(pick_ids) > 1:
+#             result['domain'] = "[('id','in',%s), ('state', '!=', 'cancel')]" % (pick_ids.ids)
+#         elif len(pick_ids) == 1:
+#             res = self.env.ref('stock.view_picking_form', False)
+#             result['views'] = [(res and res.id or False, 'form')]
+#             result['res_id'] = pick_ids.id
+#         return result
 
     
     def action_push_2_platform(self):
